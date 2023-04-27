1fcf594df89e851b38859aaae03b5c9a46dc711e
David Inglis <dinglis@conestogac.on.ca>
2023-04-27 18:44:34 +0000

    Merged in delete-cloud-save-file (pull request #5)
    Update Cloud Save to reflect new API and "delete" action
    
    * Exposing the "Delete" action from the AzureSaveSystem
    
    * 'Success' status logic is inverted and needs to be changed in the CloudSave package
    
    * CORE-83 - Updating Cloud Save package to resolve issues with the delete action
    
    * Uses new API endpoint and event parameters
    
    * Save Handler extends from the template provided by Cloud Save
    
    
    Approved-by: Anzhelika Kostyuk

---
+++ Assets/Scripts/CloudSave/SandboxSaveHandler.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.Events;
+using VARLab.CloudSave;
+
+/// <summary>
+///     This save handler wraps the Complete events exposed by the 
+///     <see cref="ExperienceSaveHandler"/> and extends them to
+///     explicit actions for Success and Failure.
+/// </summary>
+public class SandboxSaveHandler : ExperienceSaveHandler
+{
+
+    [Header("Events")]
+    public UnityEvent OnLoadSuccess;
+    public UnityEvent OnLoadFailed;
+
+    public UnityEvent OnSaveSuccess;
+    public UnityEvent OnSaveFailed;
+
+    public UnityEvent OnDeleteSuccess;
+    public UnityEvent OnDeleteFailed;
+
+
+    [Header("Startup")]
+    [Tooltip("The application should load data on startup")]
+    [SerializeField] protected bool loadOnStartup = false;
+
+
+    // Properties
+
+    public ICloudSerializer Serializer
+    {
+        get { 
+            m_Serializer ??= new JsonCloudSerializer();
+            return m_Serializer;
+        }
+    }
+
+    // Methods
+
+    public void Start()
+    {       
+        if (loadOnStartup) { Load(); }
+
+        OnSaveComplete.AddListener((response) =>
+        {
+            if (response) { OnSaveSuccess?.Invoke(); }
+            else { OnSaveFailed?.Invoke(); }
+        });
+
+        OnLoadComplete.AddListener((response) =>
+        {
+            if (response) { OnLoadSuccess?.Invoke(); }
+            else { OnLoadFailed?.Invoke(); }
+        });
+
+        OnDeleteComplete.AddListener((response) =>
+        {
+            if (response) { OnDeleteSuccess?.Invoke(); }
+            else { OnDeleteFailed?.Invoke(); }
+        });
+    }
+
+}
