749810c37b2b58de974f7987b18113ec494b1e69
David Inglis <dinglis@conestogac.on.ca>
2024-07-31 17:46:27 +0000

    Merged in CORE-867-unity-6-upgrade (pull request #94)
    Unity version upgrade to 6000.0.12
    
    * Removed deprecated packages
    * Removed ParrelSync in favour of Unity Multiplayer Play Mode
    * Upgraded additional packages as available
    
    
    Approved-by: Jacob Martens
    Approved-by: Talon Ernst

--- Assets/Samples/Lobby Networking - Netcode/Scripts/Authentication/AuthenticationHelper.cs
+++ Assets/Samples/Lobby Networking - Netcode/Scripts/Authentication/AuthenticationHelper.cs
@@ -1,12 +1,8 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
-using Unity.Services.Authentication;
-using System.Threading.Tasks;
-
-#if UNITY_EDITOR
-using ParrelSync;
-#endif
-using System;
 
 /// <summary>
 /// This class is meant to help with anything regarding the authentication process. It is a static class
@@ -22,12 +18,6 @@ public static class AuthenticationHelper
             //Setup options variable
             var options = new InitializationOptions();
 
-            //Check if we are in the unity editor and using ParrelSync, UnityServices will see them as the same person
-            //if not accounted for
-#if UNITY_EDITOR
-            if (ClonesManager.IsClone()) options.SetProfile(ClonesManager.GetArgument());
-            else options.SetProfile("Primary");
-#endif
             try
             {
                 //Initialize with UnityServices
--- Assets/Samples/Lobby Networking - Netcode/Scripts/Services/Authentication.cs
+++ Assets/Samples/Lobby Networking - Netcode/Scripts/Services/Authentication.cs
@@ -1,11 +1,7 @@
 using System.Threading.Tasks;
-using Unity.Services.Core;
-using UnityEngine;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 
-#if UNITY_EDITOR
-using ParrelSync;
-#endif
 public static class Authentication
 {
     public static string PlayerId { get; private set; }
@@ -15,12 +11,6 @@ public static class Authentication
         if(UnityServices.State == ServicesInitializationState.Uninitialized)
         {
             var options = new InitializationOptions();
-#if UNITY_EDITOR
-            // Remove this if you don't have ParrelSync installed. 
-            // It's used to differentiate the clients, otherwise lobby will count them as the same
-            if (ClonesManager.IsClone()) options.SetProfile(ClonesManager.GetArgument());
-            else options.SetProfile("Primary");
-#endif
             await UnityServices.InitializeAsync(options);
         }
 
